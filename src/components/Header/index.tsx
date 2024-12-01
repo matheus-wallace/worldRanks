29ea2a6993938d8c9285278afd36ff3507f02185
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2024-12-01 20:31:05 -0300

    feat: add the `header component` to layout page

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,28 @@
+'use client';
+
+import Image from 'next/image';
 import React from 'react';
+import { HeaderContainer, OverlayImage } from './style';
 
 const Header = () => {
-  return <header>teste</header>;
+  return (
+    <header>
+      <HeaderContainer>
+        <Image
+          src="/assets/hero-image-wr.jpg"
+          alt="background-header"
+          layout="responsive"
+          width={1280}
+          height={720}
+          objectPosition="center"
+        />
+
+        <OverlayImage>
+          <Image src={'/assets/Logo.svg'} width={250} height={250} alt="Logo world Ranks" />
+        </OverlayImage>
+      </HeaderContainer>
+    </header>
+  );
 };
 
 export default Header;
