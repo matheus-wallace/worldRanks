bc240f8e35a56f200d4a30be686e29684e423082
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-01-18 17:40:31 -0300

    feat: add the contianer to place data on page screen

---
+++ src/app/styles.tsx
@@ -0,0 +1,16 @@
+import styled from '@emotion/styled';
+
+export const ListContainer = styled.section`
+  width: 80vw;
+  padding: 2rem;
+  z-index: 10;
+  border-radius: 7px;
+  background-color: '#282B30';
+  border: 1px solid '#6C727F';
+  display: grid;
+  grid-template-columns: 1fr 8fr;
+`;
+
+export const CountrysFilterContainer = styled.article``;
+
+export const CountrysTableContainer = styled.article``;
