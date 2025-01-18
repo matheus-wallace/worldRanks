f3ab451763a0d1debb4203c567af48eb777fd89a
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-01-18 13:29:06 -0300

    feat: new `country data hook` to share data  in  application

---
+++ src/Context/CountryContext.tsx
@@ -0,0 +1,66 @@
+'use client';
+import React, { createContext, useContext, ReactNode } from 'react';
+import useSWR from 'swr';
+
+export type Country = {
+  name: {
+    common: string;
+    official: string;
+    nativeName: {
+      [key: string]: {
+        official: string;
+        common: string;
+      };
+    };
+  };
+  flag: string;
+  flags: {
+    png: string;
+    svg: string;
+    alt: string;
+  };
+  capital: string[];
+  region: string;
+  population: number;
+  area: number;
+  currencies: {
+    [key: string]: {
+      name: string;
+      symbol: string;
+    };
+  };
+};
+
+type DataContextType = {
+  data: Country[] | null;
+  error: string | null;
+  isLoading: boolean;
+};
+
+const DataContext = createContext<DataContextType | undefined>(undefined);
+
+export const useContextCountry = (): DataContextType => {
+  const context = useContext(DataContext);
+  if (!context) {
+    throw new Error('useDataCountry must be used within a DataCountryProvider');
+  }
+  return context;
+};
+
+type DataCountryProviderProps = {
+  children: ReactNode;
+};
+
+export const DataCountryProvider = ({ children }: DataCountryProviderProps) => {
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+
+  const { data, error, isLoading } = useSWR(`https://restcountries.com/v3.1/all`, fetcher);
+
+  const value = {
+    data,
+    error,
+    isLoading,
+  };
+
+  return <DataContext.Provider value={value}>{children}</DataContext.Provider>;
+};
