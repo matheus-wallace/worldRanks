c64e92f8b38eeba03803cd03460367ea6796c763
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-01-18 21:43:46 -0300

    feat: add `themeContext with default values`

---
+++ src/Context/ThemeContext.tsx
@@ -0,0 +1,62 @@
+'use client';
+import React, { createContext, useContext } from 'react';
+import { type_primary } from '@/utils/fonts/fonts';
+
+export interface Theme {
+  fonts: {
+    type_primary: string;
+  };
+  fontSize: {
+    small: string;
+    smallxl: string;
+    regular: string;
+    large: string;
+  };
+
+  fontWeights: {
+    medium: string;
+    bold: string;
+  };
+
+  breakPoints: {
+    mobile: string;
+    tablet: string;
+    desktop: string;
+  };
+}
+
+export interface ThemeContextProps {
+  children: React.ReactNode;
+}
+
+export const theme: Theme = {
+  fonts: {
+    type_primary: type_primary.className,
+  },
+
+  fontSize: {
+    small: '0.75rem',
+    smallxl: '0.875rem',
+    regular: '1rem',
+    large: '25rem',
+  },
+
+  fontWeights: {
+    medium: '600',
+    bold: '700',
+  },
+
+  breakPoints: {
+    mobile: '640px',
+    tablet: '1024px',
+    desktop: '1280px',
+  },
+};
+
+const ThemeContext = createContext<Theme>(theme);
+
+export const useTheme = (): Theme => useContext(ThemeContext);
+
+export const ThemeProvider = ({ children }: ThemeContextProps) => {
+  return <ThemeContext.Provider value={theme}>{children}</ThemeContext.Provider>;
+};
