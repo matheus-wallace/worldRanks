0f8036dabafae0c7d73bddd04b6ec6b6eb34fc74
Matheus Wallace da Silva Pereira <matheus.wallace1@outlook.com>
2025-01-19 21:02:06 -0300

    feat: add new component FlagSkeleton

--- src/components/LoadingSkeleton/index.tsx
+++ src/components/LoadingSkeleton/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react';
 import { HStack, Skeleton, Stack } from '@chakra-ui/react';
 
-export const LoadingRowList = () => {
+export const LoadingRow = () => {
   return (
     <HStack gap={'5'} maxWidth={'50vw'}>
       <Stack data-testid="skeletonLoading" flex="1">
-        <Skeleton height={'10'} />
+        <Skeleton height={'5'} />
       </Stack>
     </HStack>
   );
+};
+
+export const FlagSkeleton = () => {
+  return <Skeleton height="40px" width={'70px'} />;
 };
